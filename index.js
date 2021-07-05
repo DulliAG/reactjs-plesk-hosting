d6fa848a88353308935c2e66f28d8b3005b6093f
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-07-05 19:11:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = 80;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -8,4 +9,4 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(80);
+app.listen(port, () => console.log(`Server is listen on port ${port}!`));
